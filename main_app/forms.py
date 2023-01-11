1cb299c133493fdf83a761bd4cd50f520a810e49
escheive <scheiveerik@gmail.com>
2023-01-11 11:14:00 -0800

    started work on a profile model to extend default django user model, identified it as a stretch goal so commented out and will return

--- main_app/forms.py
+++ main_app/forms.py
@@ -6,3 +6,11 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+
+
+# return to this as stretch, adding additional values tied to user
+# class ProfileForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Profile
+#         fields = 
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import date
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create post model
 
@@ -16,12 +18,20 @@ class Post(models.Model):
 
 # Create a profile model and link it to built-in django user model so that more info can be stored about the existing User model thats not related to authentication
 # Cited source at bottom
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.TextField(max_length=500, blank=True)
+#     location = models.CharField(max_length=30, blank=True)
+#     birth_date = models.DateField(null=True, blank=True)
 
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
 # Profile model was created based on tutorial from this site:
 # https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
