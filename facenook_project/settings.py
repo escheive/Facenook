32e7f8289e7fa3eab16c81cd39f09319ed95d70c
escheive <scheiveerik@gmail.com>
2023-01-20 14:53:09 -0800

    changed allowed host name to .herokuapp.com

--- facenook_project/settings.py
+++ facenook_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True if os.environ['MODE'] == 'dev' else False
 
-ALLOWED_HOSTS = ['.heroku.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
