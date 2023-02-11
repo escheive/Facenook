e5539f7657572cd05fdca97969028022523c53f6
escheive <scheiveerik@gmail.com>
2023-02-10 16:10:47 -0800

    added a staticfile directory in settings.py

--- facenook_project/settings.py
+++ facenook_project/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = BASE_DIR/'staticfiles/'
 
+STATICFILES_DIRS = [BASE_DIR/'theme/static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
